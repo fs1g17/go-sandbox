a6a791b430979bc5414be3e7168c9be2921fe4b9
fs1g17 <fs1g17@soton.ac.uk>
2026-04-29 19:01:27 +0400

    refactor: split up into functions and move to separate file

---
+++ back2/docker.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"path/filepath"
+
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/client"
+)
+
+func createContainer(apiClient *client.Client) (client.ContainerCreateResult, error) {
+	codeDir, err := filepath.Abs("./code")
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := apiClient.ContainerCreate(
+		context.Background(),
+		client.ContainerCreateOptions{
+			Name:  "golang_test",
+			Image: "test-go-sandbox:latest",
+			Config: &container.Config{
+				Cmd: []string{"go", "run", "."},
+			},
+			HostConfig: &container.HostConfig{
+				Binds: []string{codeDir + ":/home/sandbox"},
+			},
+		})
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+type ContainerLogs struct {
+	stdout string
+	stderr string
+}
+
+func getContainerLogs(apiClient *client.Client, result client.ContainerCreateResult) (*ContainerLogs, error) {
+	apiClient.ContainerStart(context.Background(), result.ID, client.ContainerStartOptions{})
+
+	waitResult := apiClient.ContainerWait(context.Background(), result.ID, client.ContainerWaitOptions{Condition: container.WaitConditionNotRunning})
+
+	select {
+	case err := <-waitResult.Error:
+		return nil, err
+	case <-waitResult.Result:
+		fmt.Println("completed successfully")
+	}
+
+	logs, err := apiClient.ContainerLogs(context.Background(), result.ID, client.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		return nil, err
+	}
+	defer logs.Close()
+
+	var stdout, stderr bytes.Buffer
+	stdcopy.StdCopy(&stdout, &stderr, logs)
+
+	return &ContainerLogs{
+		stdout: stdout.String(),
+		stderr: stderr.String(),
+	}, nil
+}
+
+func run() {
+	apiClient, err := client.New(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	defer apiClient.Close()
+
+	result, err := createContainer(apiClient)
+	if err != nil {
+		panic(err)
+	}
+
+	containerLogs, err := getContainerLogs(apiClient, result)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("stdout: %s", containerLogs.stdout)
+	fmt.Printf("stderr: %s", containerLogs.stderr)
+
+	apiClient.ContainerRemove(context.Background(), result.ID, client.ContainerRemoveOptions{})
+}
